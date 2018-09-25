e0b976f5a10727ebef7999a7f540f4468b5367de
myk_eshun <dev.mike.eshun@gmail.com>
2018-09-25 02:52:30 +0000

    form validation

--- app/main.js
+++ app/main.js
@@ -5,11 +5,15 @@ const InputComponent = {
         <div class="field">
           <label>New Item</label>
           <input type="text" v-model="fields.newItem">
+          <span style="color: red;">{{ fieldErrors.newItem }}</span>
         </div>
+
         <div class="field">
           <label>Email</label>
           <input type="text" v-model="fields.email">
+          <span style="color: red;">{{ fieldErrors.emal }}</span>
         </div>
+
         <div class="field">
           <label>Urgency</label>
           <select class="ui fluid search dropdown" v-model="fields.urgency">
@@ -18,13 +22,17 @@ const InputComponent = {
             <option>Moderate</option>
             <option>Urgent</option>
           </select>
+          <span style="color: red;">{{ fieldErrors.urgency }}</span>
         </div>
+
         <div class="field">
           <div class="ui checkbox">
             <input type="checkbox" v-model="fields.termsAndConditions"/>
             <label><strong>I accept the terms and conditions</strong></label>
+          <span style="color: red;">{{ fieldErrors.termsAndConditions }}</span>
           </div>
         </div>
+
         <button class="ui button">Submit</button>
       </form>
       <div class="ui segment">
@@ -42,6 +50,12 @@ const InputComponent = {
       urgency: '',
       termsAndConditions: false,
     },
+    fieldErrors: {
+      newItem: undefined,
+      email: undefined,
+      urgency: undefined,
+      termsAndConditions: undefined,
+    },
     items: [],
   }),
   methods: {
@@ -49,10 +63,31 @@ const InputComponent = {
       this.items.push(this.newItem);
       this.newItem = ''
       evt.preventDefault();
-    }
-  },
-}
+    },
+    validateForm(fields) {
+      const errors = {};
 
+      if (!fields.newItem) { 
+        errors.newItem = 'New item required';
+      }
+      if (!fields.email && !this.isEmail(fields.email)) { 
+        errors.email = 'Email required';
+      }
+      if (!fields.urgency) { 
+        errors.urgency = 'Specify Item\'s urgency';
+      }
+      if (!fields.termsAndConditions) { 
+        errors.termsAndConditions = 'Accept Terms and Conditions to proceed';
+      }
+      
+      return errors;
+    },
+    isEmail(email) {
+      const re = /\S+@\S+\.S+/;
+      return re.test(email);
+    }
+  }
+}
 
 
 new Vue({
