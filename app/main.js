29db2fa95e91a8ad4b9b650e9a9778caebd4e7e3
myk_eshun <dev.mike.eshun@gmail.com>
2018-09-25 01:00:46 +0000

    basic button component display respective output on user click

--- app/main.js
+++ app/main.js
@@ -1,3 +1,31 @@
+const ButtonRow = {
+  template: `
+    <div>
+      <button @click="onHoodieClick" class="ui button">Hoodie</button>
+      <button @click="onTeeClick" class="ui button">Tee</button>
+      <button @click="onFittedCapClick" class="ui button">Fitted Cap</button>
+      <button @click="onJacketClick" class="ui button">Jacket</button>
+    </div>
+  `,
+  methods: {
+    onHoodieClick() {
+      console.log('Hoodie');
+    },
+    onFittedCapClick() {
+      console.log('Fitted Cap');
+    },
+    onJacketClick() {
+      console.log('Jacket');
+    },
+    onTeeClick() {
+      console.log('Tee');
+    },
+  },
+}
+
 new Vue({
   el: '#app',
+  components: {
+    'button-row': ButtonRow,
+  }
 })
